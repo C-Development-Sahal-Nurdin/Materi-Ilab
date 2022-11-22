f348e07671a8d8909ccd806350eb744b6ee4ecc1
hp <sahalnurdin@gmail.com>
2022-11-22 09:38:23 +0700

    add changes

--- KOMENTAR/Komentar/main.c
+++ KOMENTAR/Komentar/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+// Mencetak output dengan statement printf
+printf("Hallo, Selamat pagi");
+return 0;
 }
